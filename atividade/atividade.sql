f1ffd12317f91309d8b69b337bbf28322e49dfff
Kakanosh <kauavicente2016@gmail.com>
2026-04-17 11:26:23 -0300

    começo da atividade do slide do professor

--- atividade/atividade.sql
+++ atividade/atividade.sql
@@ -43,6 +43,38 @@ CREATE TABLE  IF NOT EXISTS livros (
    preco NUMERIC(10,2) NOT NULL,
    autor_id INTEGER REFERENCES autores(id) ON DELETE RESTRICT
 );
+
+-- atividade: Tabela Sistema de clínica Médica
+CREATE TABLE consultas (
+    id INTEGER,
+    paciente VARCHAR(100),
+    medico VARCHAR(100),
+    data_consulta TIMESTAMP,
+    valor REAL,
+    status VARCHAR(50)
+)
+CREATE TABLE consultas_certas (
+    id SERIAL PRIMARY KEY,
+    paciente_id VARCHAR REFERENCES paciente(id) ON DELETE RESTRICT,
+    medico_id INTEGER REFERENCES medico(id) ON DELETE RESTRICT,
+    data_consulta TIMESTAMP,
+    valor NUMERIC(),
+    status VARCHAR(50)
+)
+
+CREATE TABLE paciente (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100)
+);
+
+CREATE TABLE medico (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100),
+);
+
+
+
+
 --Comando para adicionar uma coluna de uma tabela criada
 ALTER TABLE livros ADD COLUMN paginas integer;
 -- Comando para deletar uma coluna de uma tabela
