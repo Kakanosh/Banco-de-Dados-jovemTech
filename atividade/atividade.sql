5831cba892b9c8f6f7253c2d6d0469b591b29d7c
Kakanosh <kauavicente2016@gmail.com>
2026-04-17 10:27:29 -0300

    adicionando comentários

--- atividade/atividade.sql
+++ atividade/atividade.sql
@@ -1,24 +1,51 @@
-CREATE TABLE IF NOT EXISTS clientes(
-  id SERIAL PRIMARY KEY,
-  nome VARCHAR(100) NOT NULL,
-  email VARCHAR(255) UNIQUE NOT NULL,
-  status VARCHAR(20) DEFAULT 'ativo',
-  limite NUMERIC(10,2) CHECK (LIMITE >=0),
-  criado_em TIMESTAMPTZ DEFAULT NOW()
+--select = seleciona as tabela
+--from = é de onde ele vai puxar
+--"*" = seleciona todos as colunas 
+--limit = ele limita a quantidade de itens que serão mostrados
+--"," ele separa cada item ou elementos que você for puxar
+
+--select * from customer limit 5
+
+--select first_name, country from customer limit 6
+
+-- eu consigo puxar as colunas pondo os nomes delas entre from e limit
+
+-- select first_name, last_name, city, email, phone from customer 
+
+-- IF NOT EXISTS = serve para criar a tabela caso ela não exita. Isso garante que não quebre seu codigo.
+
+--PK(Primary Key) chave única ou identificador único
+
+-- fg(foreign key) Chave filho que herda da chave pk pai
+
+--Serial auto incrementa ids
+CREATE TABLE IF NOT EXISTS clientes (
+
+id SERIAL PRIMARY KEY,
+nome VARCHAR(100) NOT NULL,
+email VARCHAR(255) UNIQUE NOT NULL,
+status VARCHAR(20) DEFAULT 'ativo',
+limite NUMERIC (10,2) CHECK (limite >= 0),
+criado_em TIMESTAMPTZ DEFAULT NOW()
 );
 
--- Tabela pai
-CREATE TABLE IF NOT EXISTS autores (
-  id SERIAL PRIMARY KEY,
-  nome VARCHAR(100) NOT null
+
+--PRIMEIRO CRIA A TABELA PAI
+CREATE TABLE  IF NOT EXISTS autores (
+   id SERIAL PRIMARY KEY,
+   nome VARCHAR(100) NOT NULL
 );
 
--- Depois a tabela filha (que faz a referência)
-CREATE TABLE IF NOT EXISTS livros(
-  id SERIAL PRIMARY KEY,
-  titulo VARCHAR(150) NOT NULL,
-  preco NUMERIC(10,2) NOT NULL,
-  autor_id INTEGER REFERENCES autores(id) ON DELETE RESTRICT
-);-- Na tabela track a coluna album_id uma foreign key se conecta a tabela album onde o pk é o album_id e a tabela album se conecta com a tabela artist pelo artist_id que é o foreign key com a tabela artist que tem a pk como artist_id
-
-ALTER TABLE livros ALTER COLUMN titulo type VARCHAR(200);
+--Depois a tabela filha (que faz referência)
+CREATE TABLE  IF NOT EXISTS livros (
+   id SERIAL PRIMARY KEY,
+   titulo VARCHAR(150) NOT NULL,
+   preco NUMERIC(10,2) NOT NULL,
+   autor_id INTEGER REFERENCES autores(id) ON DELETE RESTRICT
+);
+--Comando para adicionar uma coluna de uma tabela criada
+ALTER TABLE livros ADD COLUMN paginas integer;
+-- Comando para deletar uma coluna de uma tabela
+ALTER TABLE livros DROP COLUMN paginas;
+-- Comando para alterar tipo de uma coluna de uma tabela especifica
+ALTER TABLE livros ALTER COLUMN titulo TYPE VARCHAR(200);
